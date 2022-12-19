5c706d7d95e2276dda346e26648be290e1589be1
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-12-19 19:15:47 +0330

    adding event sourcing handler

---
+++ src/SM.Post/Post.Command/Post.Command.Infrastructure/Handlers/EventSourcingHandler.cs
@@ -0,0 +1,43 @@
+﻿using CQRS.Core.Domain;
+using CQRS.Core.Events;
+using CQRS.Core.Handlers;
+using CQRS.Core.Infrastructure;
+using Post.Command.Domain.Aggregates;
+
+namespace Post.Command.Infrastructure.Handlers;
+
+public class EventSourcingHandler  : IEventSourcingHandler<PostAggregate>
+{
+    private readonly IEventStore _eventStore;
+
+    public EventSourcingHandler(IEventStore eventStore)
+    {
+        _eventStore = eventStore;
+    }
+
+    public async Task SaveAsync(AggregateRoot aggregate)
+    {
+        await _eventStore.SaveEventAsync(aggregate.Id,
+            aggregate.GetUncommittedChanges(), 
+            aggregate.Version);
+        aggregate.MarkChangesAsCommitted();
+    }
+
+    public async Task<PostAggregate> GetByIdAsync(Guid aggregateId)
+    {
+        PostAggregate aggregate = new();
+        List<BaseEvent> events = await _eventStore.GetEventAsync(aggregateId);
+
+        if (events == null || !events.Any())
+        {
+            return aggregate;
+        }
+        
+        aggregate.ReplayEvent(events);
+        aggregate.Version = events
+            .Select(x => x.Version)
+            .Max()!.Value;
+
+        return aggregate;
+    }
+}
