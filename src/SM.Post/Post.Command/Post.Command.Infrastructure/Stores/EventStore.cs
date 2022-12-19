fa476ad518145b54389184bbbb07307fe6f26cd4
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-12-19 19:16:01 +0330

    adding event store

---
+++ src/SM.Post/Post.Command/Post.Command.Infrastructure/Stores/EventStore.cs
@@ -0,0 +1,64 @@
+﻿using CQRS.Core.Domain;
+using CQRS.Core.Events;
+using CQRS.Core.Exceptions;
+using CQRS.Core.Infrastructure;
+using Post.Command.Domain.Aggregates;
+
+namespace Post.Command.Infrastructure.Stores;
+
+public class EventStore : IEventStore
+{
+    private readonly IEventStoreRepository _eventStoreRepository;
+
+    public EventStore(IEventStoreRepository eventStoreRepository)
+    {
+        _eventStoreRepository = eventStoreRepository;
+    }
+
+    public async Task SaveEventAsync(Guid aggregateId,
+        IEnumerable<BaseEvent> events,
+        int expectedVersion)
+    {
+        List<EventModel>? eventStream = await _eventStoreRepository
+            .FindByAggregateId(aggregateId);
+
+        if (expectedVersion != -1 && eventStream[^1].Version != null)
+        {
+            throw new ConcurrencyException();
+        }
+
+        int version = expectedVersion;
+
+        foreach (BaseEvent @event in events)
+        {
+            version++;
+            @event.Version = version;
+            string eventType = @event.GetType().Name;
+            EventModel eventModel = new()
+            {
+                TimeStamp = DateTime.Now,
+                AggregateIdentifier = aggregateId,
+                AggregateType = nameof(PostAggregate),
+                Version = version,
+                EventType = eventType,
+                EventData = @event
+            };
+
+            await _eventStoreRepository.SaveAsync(eventModel);
+        }
+    }
+
+    public async Task<List<BaseEvent>> GetEventAsync(Guid aggregateId)
+    {
+        List<EventModel>? eventStream = await _eventStoreRepository
+            .FindByAggregateId(aggregateId);
+
+        if (eventStream == null || !eventStream.Any())
+        {
+            throw new AggregateNotFoundException("Incorrect post ID provided");
+        }
+
+        return eventStream!.OrderBy(x => x.Version)
+            .Select(x => x.EventData).ToList()!;
+    }
+}
