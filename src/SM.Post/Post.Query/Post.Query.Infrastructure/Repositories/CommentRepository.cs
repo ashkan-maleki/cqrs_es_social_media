fab841bc8747b499c51d92e81827efecef5c18e7
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-12-22 16:47:02 +0330

    adding comment repository

---
+++ src/SM.Post/Post.Query/Post.Query.Infrastructure/Repositories/CommentRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Entities;
+using Post.Query.Domain.Repositories;
+using Post.Query.Infrastructure.DataAccess;
+
+namespace Post.Query.Infrastructure.Repositories;
+
+public class CommentRepository : ICommentRepository
+{
+    private readonly DatabaseContextFactory _contextFactory;
+
+    public CommentRepository(DatabaseContextFactory contextFactory)
+    {
+        _contextFactory = contextFactory;
+    }
+
+    public async Task CreateAsync(CommentEntity comment)
+    {
+        await using DatabaseContext context = _contextFactory.CreateDbContext();
+        context.Comments.Add(comment);
+        _ = await context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(CommentEntity comment)
+    {
+        await using DatabaseContext context = _contextFactory.CreateDbContext();
+        context.Update(comment);
+        _ = await context.SaveChangesAsync();
+    }
+
+    public async Task<CommentEntity> GetByIdAsync(Guid commentId)
+    {
+        await using DatabaseContext context = _contextFactory.CreateDbContext();
+
+        return await context.Comments.AsNoTracking()
+            .FirstOrDefaultAsync(c => c.CommentId == commentId);
+
+    }
+
+    public async Task DeleteAsync(Guid commentId)
+    {
+        await using DatabaseContext context = _contextFactory.CreateDbContext();
+
+        CommentEntity comment = await context.Comments
+                .FirstOrDefaultAsync(c => c.CommentId == commentId);
+
+        if (comment == null)
+        {
+            return;;
+        }
+        
+        context.Comments.Remove(comment);
+        _ = await context.SaveChangesAsync();
+    }
+}
