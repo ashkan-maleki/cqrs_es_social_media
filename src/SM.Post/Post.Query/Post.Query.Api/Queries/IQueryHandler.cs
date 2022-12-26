ad969d555c477635258a6dbdbb09069d706ed6bd
Mamal Maleki <mohammadalimalekie@gmail.com>
2022-12-26 17:48:01 +0330

    develop query handler

---
+++ src/SM.Post/Post.Query/Post.Query.Api/Queries/IQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Formats.Asn1;
+using Post.Query.Domain.Entities;
+
+namespace Post.Query.Api.Queries;
+
+public interface IQueryHandler
+{
+    Task<List<PostEntity>> HandleAsync(FindAllPostQuery query);
+    Task<List<PostEntity>> HandleAsync(FindPostByIdQuery query);
+    Task<List<PostEntity>> HandleAsync(FindPostByAuthorQuery query);
+    Task<List<PostEntity>> HandleAsync(FindPostWithCommentQuery query);
+    Task<List<PostEntity>> HandleAsync(FindPostWithLikesQuery query);
+}
---
+++ src/SM.Post/Post.Query/Post.Query.Api/Queries/QueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Post.Query.Domain.Entities;
+using Post.Query.Domain.Repositories;
+
+namespace Post.Query.Api.Queries;
+
+public class QueryHandler : IQueryHandler
+{
+    private readonly IPostRepository _postRepository;
+
+    public QueryHandler(IPostRepository postRepository) 
+        => _postRepository = postRepository;
+
+    public async Task<List<PostEntity>> HandleAsync(FindAllPostQuery query) 
+        => await _postRepository.ListAllAsync();
+
+    public async Task<List<PostEntity>> HandleAsync(FindPostByIdQuery query)
+    {
+        PostEntity? post = await _postRepository.GetByIdAsync(query.Id!.Value);
+        return new List<PostEntity> {post};
+    }
+
+    public async Task<List<PostEntity>> HandleAsync(FindPostByAuthorQuery query) 
+        => await _postRepository.ListByAuthorAsync(query.Author);
+
+    public async Task<List<PostEntity>> HandleAsync(FindPostWithCommentQuery query) 
+        => await _postRepository.ListWithCommentsAsync();
+
+    public async Task<List<PostEntity>> HandleAsync(FindPostWithLikesQuery query) 
+        => await _postRepository.ListWithLikesAsync(query.NumberOfLikes!.Value);
+}
