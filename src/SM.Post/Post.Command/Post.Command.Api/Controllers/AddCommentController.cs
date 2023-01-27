559be235c8d9a6e23b5b9466f0b742fd02fd88a9
Mamal Maleki <mohammadalimalekie@gmail.com>
2023-01-27 14:19:32 +0330

    Add comment controller

---
+++ src/SM.Post/Post.Command/Post.Command.Api/Controllers/AddCommentController.cs
@@ -0,0 +1,18 @@
+﻿using CQRS.Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Post.Command.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/add-comment")]
+public class AddCommentController : ControllerBase
+{
+    private readonly ILogger<LikePostController> _logger;
+    private readonly ICommandDispatcher _commandDispatcher;
+    
+    public AddCommentController(ILogger<LikePostController> logger, ICommandDispatcher commandDispatcher)
+    {
+        _logger = logger;
+        _commandDispatcher = commandDispatcher;
+    }
+}
